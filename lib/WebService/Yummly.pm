4523d691b6d7e379d4edda1aa9014d6e1a609756
Dave Hodgkinson <daveh@hodgkinson.org>
2015-04-07 12:26:47 +0100

    spiffed up test and release

--- lib/WebService/Yummly.pm
+++ lib/WebService/Yummly.pm
@@ -27,8 +27,8 @@ sub new {
        response_parser => 'JSON',
        param    =>
        {
-	   _app_id  => $APP_ID,
-	   _app_key => $APP_KEY,
+        _app_id  => $APP_ID,
+        _app_key => $APP_KEY,
        }
       );
     return $obj ;
@@ -54,3 +54,79 @@ sub get_recipe {
 }
 
 1;
+
+=pod 
+
+=head1 NAME
+
+WebService::Yummly - Simple interface to the search and recipe interface to Yummly
+
+=head1 SYNOPSIS
+
+  use WebService::Yummly;
+
+  # use your ID/key here
+  my $APP_ID = "2f6cfcff";
+  my $APP_KEY = "a3cec319936cdf2fb03f4b0f5dfdaf4e";
+  my $y = WebService::Yummly->new($APP_ID, $APP_KEY);
+  my $recipes = $y->search("lamb shank");
+
+  my $r = WebService::Yummly->new($APP_ID, $APP_KEY, "Sunday-Supper_-Curried-Lamb-Shanks-Serious-Eats-42000");
+  ok($r,"new yummly");
+
+  my $recipe = $r->get_recipe ;
+
+=head1 DESCRIPTION
+
+  Search and retrieve recipe from Yummly
+
+=head1 FUNCTIONS
+
+=head2 new
+
+  my $y = WebService::Yummly->new($APP_ID, $APP_KEY);
+
+Create a new Yummly object passing in credentials.
+
+=head2 search
+
+  $recipes = $y->search("lamb shank") ;
+
+Return a JSON structure containing matching recipes.
+
+=head2 get_recipe
+
+  my $r = WebService::Yummly->new($APP_ID, $APP_KEY, "Sunday-Supper_-Curried-Lamb-Shanks-Serious-Eats-42000");
+  my $recipe = $r->get_recipe ;
+
+Return a JSON data structure with recipe information.
+
+=head1 DIAGNOSTICS
+
+=head1 SUPPORT
+
+=head2 BUGS
+
+Please report any bugs by email to C<bug-webservice-yummly at rt.cpan.org>, or 
+through the web interface at L<http://rt.cpan.org/Public/Dist/Display.html?Name=WebService-Yummly>. 
+You will be automatically notified of any progress on the request by the system.
+
+=head2 SOURCE CODE
+
+This is open source software. The code repository is available for public 
+review and contribution under the terms of the license.
+
+L<https://github.com/davehodg/Webservice-Yummly/>
+
+    git clone https://github.com/davehodg/Webservice-Yummly
+
+=head1 AUTHOR
+
+Dave Hodgkinson C<davehodg@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright 2014 by Dave Hodgkinson
+
+This library is under the Artistic License.
+
